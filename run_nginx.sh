2ff3876a0b2943597017e7b54c616a34cd9fb8f9
Lee Harrison <leeharrison1984@gmail.com>
2017-12-26 21:38:38 -0600

    Make message more descriptive

--- run_nginx.sh
+++ run_nginx.sh
@@ -1,4 +1,4 @@
-echo "Fixing up default_location file with $HEALTH_CHECK_URL"
+echo "Fixing up default_location file with health check url: $HEALTH_CHECK_URL"
 sed -i "s!HEALTH_CHECK_URL!$HEALTH_CHECK_URL!g" /etc/nginx/vhost.d/default_location
 
 echo "Starting up NGINX"
